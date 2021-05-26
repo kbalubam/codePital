c4d7e107a836ced01e699e6cccef8038c9955136
kbalubam <kevin.baluba@hotmail.com>
2021-05-26 15:29:18 +0200

    versions finale, sorry oubli commit

--- public/classes.js
+++ public/classes.js
@@ -1,56 +1,102 @@
 export class Lieu {
     constructor(nom, personnes) {
-        this.nom=nom,
-        this.personnes=personnes
+        this.nom = nom,
+            this.personnes = personnes
     }
 }
 
-export class Patients{
-    constructor(nom,maladie,argent,poche,etatSante) {
-        this.nom=nom,
-        this.maladie=maladie,
-        this.argent=argent,
-        this.argent=argent,
-        this.poche=poche,
-        this.etatSante=etatSante
-        this.traitement = ()=>{
+export class Patients {
+    constructor(nom, maladie, argent, poche, etatSante) {
+        this.nom = nom,
+            this.maladie = maladie,
+            this.argent = argent,
+            this.argent = argent,
+            this.poche = poche,
+            this.etatSante = etatSante
+        this.traitement = (medicament) => {
+            console.log(`le traitement de Marcus coûte ${medicament.prix}`);
+            if (this.argent >= medicament.prix) {
+                this.poche.push(medicament.nom);
+                this.paye(medicament);
+                this.takeCare();
+
+            } else {
+                console.log(`${this.nom} n'as pas assez d'argent`);
+                console.log(`${this.nom} est mort et est au cimetière`)
+                
+
+            }
 
         }
-        this.gotTo = ()=>{
+        this.gotTo = (lieu) => {
+
+
+
+            lieu.personnes.push(this.nom);
+            console.log(`${this.nom} est allé à la ${lieu.nom}`)
 
         }
-        this.paye = () =>{
 
+
+
+        this.takeCare = () => {
+            this.etatSante = "soigné"
+            console.log(`${this.nom} est ${this.etatSante}`);
+        }
+
+        this.paye = (medicament) => {
+            this.argent -= medicament.prix;
         }
     }
 }
 
 export class Doctor {
-    constructor(nom, argent , cabinet, diagnostique, patientIn, patientOut) {
-      this.nom=nom,
-      this.argent=argent,
-      this.cabinet=cabinet,
-      
-      this.patientIn = () =>{
+    constructor(nom, argent, cabinet) {
+        this.nom = nom,
+            this.argent = argent,
+            this.cabinet = cabinet,
 
-      }
-      this.patientOut = () =>{
 
-      }
-      
+
+            this.patientIn = (patient, salle) => {
+                salle.personnes.splice(salle.personnes.indexOf(patient.nom), 1);
+                this.cabinet.push(patient.nom);
+                console.log(`${patient.nom} est entré dans le cabinet`)
+            }
+
+        this.diagnostique = (patient, maladieD) => {
+            console.log(`la maladie de ${patient.nom} est ${patient.maladie}`);
+            if (patient.argent >= 50) {
+                this.argent += 50;
+                patient.argent-=50;
+                console.log(`la consulation est payée, le medecin va lui prescrire un traitement`);
+                console.log(`Le traitement de ${patient.nom} est ${maladieD.traitement}`)
+
+            } else {
+                console.log(`Le medecin appelle la police et embarque ${patient.nom}`);
+
+            }
+        }
+
+
+        this.patientOut = (patient) => {
+            this.cabinet.splice(this.cabinet.indexOf(patient.nom), 1);
+            console.log(`${patient.nom} est sorti du cabinet`)
+        }
+
     }
 }
 
- export class Diagnostique{
-    constructor(maladie,traitement){
-        this.maladie=maladie,
-        this.traitement=traitement
+export class Diagnostique {
+    constructor(maladie, traitement) {
+        this.maladie = maladie,
+            this.traitement = traitement
     }
 }
 
-export class Traitement{
-    constructor(nom,prix) {
-        this.nom=nom,
-        this.prix=prix
+export class Traitement {
+    constructor(nom, prix) {
+        this.nom = nom,
+            this.prix = prix
     }
 }
--- public/main.js
+++ public/main.js
@@ -1,12 +1,5 @@
 import {Lieu, Patients, Doctor, Diagnostique, Traitement} from "./classes.js";
  
-// Definition des lieux
-
-let salle_attente = new Lieu("salle d'attente", []);
-
-let pharmacie = new Lieu("pharmacie", []);
-
-let cimetière = new Lieu("cimetière", []);
 
 // Definition des patients 
 
@@ -19,6 +12,18 @@ let sangoku = new Patients ("Sangoku", "404", 80, [], "malade");
 let darthVader = new Patients ("DarthVader", "azmatique", 110, [], "malade");
 
 let semicolon = new Patients ("Semicolon", "syntaxError", 60, [], "malade");
+
+// Definition des lieux
+
+let salle_attente = new Lieu("salle d'attente", [marcus.nom,optimus.nom,sangoku.nom,darthVader.nom,semicolon.nom]);
+let listePatient=[marcus,optimus,sangoku,darthVader,semicolon];
+
+
+let pharmacie = new Lieu("pharmacie", []);
+
+let cimetière = new Lieu("cimetière", []);
+
+
 
 // Definition du doceur 
 
@@ -34,7 +39,7 @@ let not_found = new Diagnostique("404", "CheckLinkRelation");
 
 let azmatique = new Diagnostique("azmatique", "Ventoline");
 
-let SyntaxError = new Diagnostique("syntaxError", "f12+doc");
+let syntaxError = new Diagnostique("syntaxError", "f12+doc");
 
 // Definition des traitements 
 
@@ -47,3 +52,51 @@ let check = new Traitement ("checkLinkRelation", 35);
 let ventoline = new Traitement ("ventoline", 40);
 
 let f12doc = new Traitement ("f12+doc", 20);
+
+
+
+    // Marcus
+    console.log(`Dans la ${salle_attente.nom} il y a ${salle_attente.personnes.length} personnes`);
+    Debugger.patientIn(marcus, salle_attente);
+    Debugger.diagnostique(marcus,mal_indenté);
+    Debugger.patientOut(marcus);
+    marcus.gotTo(pharmacie);
+    marcus.traitement(ctrl);
+    console.log("-----------");
+
+    console.log(`Dans la ${salle_attente.nom} il y a ${salle_attente.personnes.length} personnes`);
+    Debugger.patientIn(optimus , salle_attente);
+    Debugger.diagnostique(optimus ,unsave);
+    Debugger.patientOut(optimus);
+    optimus.gotTo(pharmacie);
+    optimus.traitement(save);
+    console.log("-----------");
+
+    console.log(`Dans la ${salle_attente.nom} il y a ${salle_attente.personnes.length} personnes`);
+    Debugger.patientIn(sangoku , salle_attente);
+    Debugger.diagnostique(sangoku,not_found);
+    Debugger.patientOut(sangoku);
+    sangoku.gotTo(pharmacie);
+    sangoku.traitement(check);
+    console.log("-----------");
+
+
+
+    console.log(`Dans la ${salle_attente.nom} il y a ${salle_attente.personnes.length} personnes`);
+    Debugger.patientIn(darthVader , salle_attente);
+    Debugger.diagnostique(darthVader,azmatique);
+    Debugger.patientOut(darthVader);
+    darthVader.gotTo(pharmacie);
+    darthVader.traitement(ventoline);
+    console.log("-----------");
+    
+
+
+    console.log(`Dans la ${salle_attente.nom} il y a ${salle_attente.personnes.length} personnes`);
+    Debugger.patientIn(semicolon , salle_attente);
+    Debugger.diagnostique(semicolon,syntaxError);
+    Debugger.patientOut(semicolon);
+    semicolon.gotTo(pharmacie);
+    semicolon.traitement(f12doc);
+    console.log("-----------");
+    
